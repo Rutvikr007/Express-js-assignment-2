9e932ae054a847eafcf95a5915236e66aacf3bfe
RUTVIK  RAMTEKE <95290638+Rutvikr007@users.noreply.github.com>
2022-07-05 21:12:05 +0530

    Add files via upload

---
+++ controllers/stuListController.js
@@ -0,0 +1,19 @@
+const results = [
+    "Rajesh",
+    "Ramesh",
+    "Sayali",
+    "Sanjana"
+
+]
+
+const studentList = (req, res) => {
+
+    res.status(200).json({
+
+        "results": results
+
+    });
+
+};
+
+module.exports = studentList;
